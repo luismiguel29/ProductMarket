5ad0aa969d52c6932cd258661ef84cf666942256
luismiguel29 <rojashuallcoluismiguel29@gmail.com>
2022-10-18 23:55:15 -0400

    vilmacambios

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProductoVilma;
+use App\Http\Controllers\ProductoVilmaController;
 
 /*
 |--------------------------------------------------------------------------
