193c159f22106bb71b8a9f74b2cda1038f39abd5
ypelaez <yohana.pelaez.cruz@gmail.com>
2022-10-19 11:08:40 -0400

    [UPDATE] views

--- resources/views/registrar.blade.php
+++ resources/views/registrar.blade.php
@@ -21,9 +21,9 @@
 
     <div class="d-flex justify-content-around">
         <section class="d-flex flex-column  align-self-center gap-4">
-            <button type="button" class="btn btn-dark ">Editar</button>
-            <a type="button" class="btn btn-dark btn-2 " style="background-color: #575555;">Registrar</a>
-            <button type="button" class="btn btn-dark">Ver productos</button>
+            <button type="button" class="btn btn-secondary fs-5">Editar</button>
+            <a type="button" class="btn btn-secondary fs-5">Registrar</a>
+            <button type="button" class="btn btn-secondary fs-5">Ver productos</button>
         </section>
 
         <div class="card border-dark mb-3" style="width: 30rem;">
@@ -90,8 +90,8 @@
 
                     <!--<button type="submit" class="btn btn-secondary botton1">Registrar</button>-->
                     <div class="d-flex justify-content-evenly ">
-                        <button type="submit" class="btn btn-primary botton1">Registrar</button>
-                       <!-- <a type="button" href="" class="btn btn-danger botton2">Cancelar</a>-->
+                        <button type="submit" class="btn btn-secondary fs-5 ">Registrar</button>
+                       <a type="button" href="" class="btn btn-secondary fs-5">Cancelar</a>
                     </div>
 
 
