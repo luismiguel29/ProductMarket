47ba2a42b0124ce6b570397a63a08932d41db79a
AABR2001 <adrianaboza0@gmail.com>
2022-10-20 21:42:03 -0400

    prueba

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-    <h1>hola</h1>
+    <h1>hola como estas</h1>
     <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
