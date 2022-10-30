b9de796940dd80c90935138065e8f1eaf5b5fff0
ypelaez <yohana.pelaez.cruz@gmail.com>
2022-10-30 16:56:20 -0400

    [CREATE] estilos y personalizacióna los cards

--- resources/views/Cliente/novedades.blade.php
+++ resources/views/Cliente/novedades.blade.php
@@ -1,41 +1,139 @@
 @extends('layouts.template')
 
 @section('content')
+    <div>
+        <h1>CATEGORIAS</h1>
+
+    </div>
     <div
-    <div class="row row-cols-1 row-cols-md-3 g-4">
+    <div class=" d-flex justify-content-evenly row row-cols-1 row-cols-md-5 g-1">
         <div class="col">
           <div class="card h-100">
-            <img src="..." class="card-img-top" alt="...">
+            <div class="card-header c-header">
+              <small class="text-center-">SUPER OFERTA</small>
+            </div>
             <div class="card-body">
               <h5 class="card-title">Card title</h5>
               <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
             </div>
-            <div class="card-footer">
-              <small class="text-muted">Last updated 3 mins ago</small>
+            <div class="card-footer c-footer">
+              <small class="text-muted">Last updated 3  </small>
             </div>
           </div>
         </div>
         <div class="col">
           <div class="card h-100">
-            <img src="..." class="card-img-top" alt="...">
+            <div class="card-header c-header">
+              <small class="text-center">SUPER OFERTA</small>
+            </div>
             <div class="card-body">
               <h5 class="card-title">Card title</h5>
               <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
             </div>
-            <div class="card-footer">
-              <small class="text-muted">Last updated 3 mins ago</small>
+            <div class="card-footer c-footer">
+              <small class="text-muted">Last updated 3 </small>
             </div>
           </div>
         </div>
         <div class="col">
           <div class="card h-100">
-            <img src="..." class="card-img-top" alt="...">
+            <div class="card-header c-header">
+              <small class="text-center">SUPER OFERTA</small>
+            </div>
             <div class="card-body">
               <h5 class="card-title">Card title</h5>
               <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
             </div>
-            <div class="card-footer">
-              <small class="text-muted">Last updated 3 mins ago</small>
+            <div class="card-footer c-footer">
+              <small class="text-muted">Last updated 3</small>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class=" d-flex justify-content-evenly row row-cols-1 row-cols-md-5 g-1">
+        <div class="col">
+          <div class="card h-100">
+            <div class="card-header c-header">
+              <small class="text-center-">SUPER OFERTA</small>
+            </div>
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+            <div class="card-footer c-footer">
+              <small class="text-muted">Last updated 3  </small>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card h-100">
+            <div class="card-header c-header">
+              <small class="text-center">SUPER OFERTA</small>
+            </div>
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+            </div>
+            <div class="card-footer c-footer">
+              <small class="text-muted">Last updated 3 </small>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card h-100">
+            <div class="card-header c-header">
+              <small class="text-center">SUPER OFERTA</small>
+            </div>
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+            </div>
+            <div class="card-footer c-footer">
+              <small class="text-muted">Last updated 3</small>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class=" d-flex justify-content-evenly row row-cols-1 row-cols-md-5 g-1">
+        <div class="col">
+          <div class="card h-100">
+            <div class="card-header c-header">
+              <small class="text-center-">SUPER OFERTA</small>
+            </div>
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+            <div class="card-footer c-footer">
+              <small class="text-muted">Last updated 3  </small>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card h-100">
+            <div class="card-header c-header">
+              <small class="text-center">SUPER OFERTA</small>
+            </div>
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+            </div>
+            <div class="card-footer c-footer">
+              <small class="text-muted">Last updated 3 </small>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card h-100">
+            <div class="card-header c-header">
+              <small class="text-center">SUPER OFERTA</small>
+            </div>
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+            </div>
+            <div class="card-footer c-footer">
+              <small class="text-muted">Last updated 3</small>
             </div>
           </div>
         </div>
