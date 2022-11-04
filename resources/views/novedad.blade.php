448ecd51261d91cbdfd3212781fcbbe19750a293
VictorApaza <victorapazac12345@gmail.com>
2022-11-04 19:59:30 -0400

    MEJORAS EN BARRA DE MENUS Y EN LAS TARJETAS DE NOVEDADES

--- resources/views/novedad.blade.php
+++ resources/views/novedad.blade.php
@@ -11,87 +11,128 @@
 </head>
 <header>
 <nav class="navbar bg-white fixed-top">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#"></a>
-    <button class="navbar-toggler navbar-toggler-start" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-      <div class="offcanvas-header">
-        <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-      </div>
-      <div class="offcanvas-body">
-        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
-          </li>
-        </ul>   
+    <div class="container-fluid">
+      <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar"
+        aria-controls="offcanvasNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+        <div class="offcanvas-header">
+          <h5 class="offcanvas-title" id="offcanvasNavbarLabel">MENU</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        </div>
+        <div class="offcanvas-body">
+          <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+        </div>
       </div>
     </div>
-  </div>
-</nav>
+  </nav>
 </header>
 
 <body>
-<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-  <div class="carousel-inner">
-  <div class="carousel-item active">
-
-<section class="container mt-20 pt-20 bottom-0">
-<div class="row justify-content-center align-items-center vh-100 ">
-    <!-- espacio 1-->
-    <div class="col">
-      <div class="card" style="width:18rem;">
-        <div class="card-header c-header ">SUPER OFERTA</div>
-        <img src="..." class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">TITULO</h5>
-        </div>
-        <div class="card-footer c-footer">PRECIO</div>
-        
-      </div>
-    </div>
-    
-
-        <!-- espacio 1-->
-        <div class="col">
-          <div class="card" style="width:18rem;">
-            <div class="card-header c-header">SUPER OFERTA</div>
-            <img src="..." class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">TITULO</h5>
+<div class="container">
+    <div id="carouselExampleControls" class="carousel carousel-dark slide" data-ride="carousel">
+      <div class="carousel-inner">
+        <div class="carousel-item active" data-bs-interval="3000">
+          <div class="cards-wrapper">
+            <div class="card">
+              <div class="card-header c-header">SUPER OFERTA</div>
+              <img src="..." class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">TITULO</h5>
+              </div>
+              <div class="card-footer c-footer">PRECIO</div>
             </div>
-            <div class="card-footer c-footer">PRECIO</div>
+            <div class="card">
+              <div class="card-header c-header">SUPER OFERTA</div>
+              <img src="..." class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">TITULO</h5>
+              </div>
+              <div class="card-footer c-footer">PRECIO</div>
+            </div>
+            <div class="card">
+              <div class="card-header c-header">SUPER OFERTA</div>
+              <img src="..." class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">TITULO</h5>
+              </div>
+              <div class="card-footer c-footer">PRECIO</div>
+            </div>
           </div>
         </div>
-
-            <!-- espacio 1-->
-    <div class="col">
-      <div class="card" style="width:18rem;">
-        <div class="card-header c-header">SUPER OFERTA</div>
-        <img src="..." class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">TITULO</h5>
+        <div class="carousel-item" data-bs-interval="3000">
+          <div class="cards-wrapper">
+            <div class="card">
+              <div class="card-header c-header">SUPER OFERTA</div>
+              <img src="..." class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">TITULO</h5>
+              </div>
+              <div class="card-footer c-footer">PRECIO</div>
+            </div>
+            <div class="card">
+              <div class="card-header c-header">SUPER OFERTA</div>
+              <img src="..." class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">TITULO</h5>
+              </div>
+              <div class="card-footer c-footer">PRECIO</div>
+            </div>
+            <div class="card">
+              <div class="card-header c-header">SUPER OFERTA</div>
+              <img src="..." class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">TITULO</h5>
+              </div>
+              <div class="card-footer c-footer">PRECIO</div>
+            </div>
+          </div>
         </div>
-        <div class="card-footer c-footer">PRECIO</div>
+        <div class="carousel-item" data-bs-interval="3000">
+          <div class="cards-wrapper">
+            <div class="card">
+              <div class="card-header c-header">SUPER OFERTA</div>
+              <img src="..." class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">TITULO</h5>
+              </div>
+              <div class="card-footer c-footer">PRECIO</div>
+            </div>
+            <div class="card">
+              <div class="card-header c-header">SUPER OFERTA</div>
+              <img src="..." class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">TITULO</h5>
+              </div>
+              <div class="card-footer c-footer">PRECIO</div>
+            </div>
+            <div class="card">
+              <div class="card-header c-header">SUPER OFERTA</div>
+              <img src="..." class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">TITULO</h5>
+              </div>
+              <div class="card-footer c-footer">PRECIO</div>
+            </div>
+          </div>
+        </div>
       </div>
+      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"
+        data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls"
+        data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
     </div>
-
-
   </div>
-  </section>
-  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
-</div>
-</div></div>
-
 </body>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
   integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
