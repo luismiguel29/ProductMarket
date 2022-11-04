574102d026177f027046c905ee5f5f1be02de991
luismiguel29 <rojashuallcoluismiguel29@gmail.com>
2022-11-04 11:27:11 -0400

    prueba

--- resources/views/menu2.blade.php
+++ resources/views/menu2.blade.php
@@ -15,27 +15,12 @@
 
 <body>
   
-    <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-        <div class="carousel-inner">
-          <div class="carousel-item active">
-            <img src="" class="d-block w-100" alt="">
-          </div>
-          <div class="carousel-item">
-            <img src="" class="d-block w-100" alt="">
-          </div>
-          <div class="carousel-item">
-            <img src="" class="d-block w-100" alt="">
-          </div>
-        </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Anterior</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Siguiente</span>
-        </button>
-      </div>
+  <div>
+    hola mundo
+    <div>
+      <input type="text">
+    </div>
+  </div>
 
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
     integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
