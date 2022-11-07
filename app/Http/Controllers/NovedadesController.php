02d6583ef29f41a4a7521b3b988807a10e2e61a8
ypelaez <yohana.pelaez.cruz@gmail.com>
2022-11-07 17:54:54 -0400

    [UPDATE] correción de cloud

--- app/Http/Controllers/NovedadesController.php
+++ app/Http/Controllers/NovedadesController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Producto;
+use App\Models\producto as ModelsProducto;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Storage;
@@ -15,21 +17,53 @@ class NovedadesController extends Controller
      */
     public function index()
     {
+        
         $productos = DB::table('producto')
-            ->get();
+        ->get();
+    
+        $cont = count($productos);
+        
+        if($cont < 10 ){
+            $data = 0;
+        }else{
+            $data = count($productos)-4;
+        }
 
-        $categoria = DB::table('categoria')
-            ->orderByRaw('nombre ASC')
-            ->get();
-        //return view('menu', compact('categoria'));
+        $a=array();
+       
+        for($var = 9; $var>0;$var--){
+            $extraer = Producto::where('id_categoria',$var)->orderBy('idproducto','desc')->first();
+            if($extraer !=""){
+                array_unshift($a,$extraer);
+            }
+        }
 
-        return view('/Cliente/novedades', compact('productos', 'categoria'));
+        //$b1 = array(); $b2 = array(); $b3 = array();
+        $b = array([],[],[]);
+        $aux = 0;
+        foreach($a as $c){
+            if(count($a)%3 != 0){      
+                array_unshift($b[$aux],$c);  
+                if(count($b[$aux])==3){
+                    $aux++;
+                }
+            }
+        }
+        $b1 = $b[0];
+        $b2 = $b[1];
+        $b3 = $b[2];
+        //return $b;
+        
 
+       return view('/Cliente/novedades', compact('b','a'));
+
+
+        //return $extraer;
         /*$producto = DB::table('producto')
-    ->orderByRaw('nombre ASC')
-    ->get();
-    return $producto;
-     */
+        ->orderByRaw('nombre ASC')
+        ->get();
+        return $producto;
+        */
     }
 
     /**
@@ -39,9 +73,9 @@ class NovedadesController extends Controller
      */
     public function create()
     {
-
-        return view('producto.create');
-
+        
+            return view('producto.create');
+        
     }
 
     /**
@@ -52,7 +86,7 @@ class NovedadesController extends Controller
      */
     public function store(Request $request)
     {
-
+        //
     }
 
     /**
@@ -100,3 +134,4 @@ class NovedadesController extends Controller
         //
     }
 }
+
