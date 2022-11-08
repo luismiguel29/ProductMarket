b37d96a5a5c806679dbe1542e31c95166b57e58a
VictorApaza <victorapazac12345@gmail.com>
2022-11-07 22:43:30 -0400

    menu con logo en template y en css

--- resources/views/Cliente/novedades.blade.php
+++ resources/views/Cliente/novedades.blade.php
@@ -2,8 +2,8 @@
 
 @section('content')
     <section class="container-sm">
-            <div id="carouselExampleControls"  class="carousel carousel-dark slide car-out" data-ride="carousel"{{--  data-bs-ride="carousel" --}}>
-                <div class="carousel-inner">
+    <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
+      <div class="carousel-inner">
                     @foreach ($b as $item)   
                     <div class="carousel-item active" data-bs-interval="5000" >
                         <div class="cards-wrapper" >
@@ -38,16 +38,14 @@
                    @endforeach
                     
                 </div>
-                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"
-                    data-bs-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Previous</span>
-                </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls"
-                    data-bs-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Next</span>
-                </button>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
             </div>
 </section>
 @endsection
