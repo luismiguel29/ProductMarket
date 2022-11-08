5545225a5716d48775e9d707698bb74d39d1f76d
VilMF <vilmamamaniflores@gmail.com>
2022-11-08 18:40:47 -0400

    Actualizacion botones de la pagina proveedor

--- resources/views/Proveedor/Paginaproveedor.blade.php
+++ resources/views/Proveedor/Paginaproveedor.blade.php
@@ -7,9 +7,9 @@
   <div class="row " style="min-height:600px">
 
     <div class="btn-vertical col d-flex  flex-column align-self-center   d-flex justify-content-start" style="padding-right:15em">
-      <div class="my-3 mx-auto"><a class="btn btn-dark fs-5 " href="/datosNego" style="width:180px">Editar</a></div>
-      <div class="my-3 mx-auto"><a class="btn btn-dark fs-5" href="/categoria" style="width:180px">Registrar producto</a></div>
-      <div class="my-3 mx-auto"><a class="btn btn-dark fs-5" href="{{route('listaproducto')}}" style="width:180px">Ver productos</a></div>
+      <div class="my-3 mx-auto"><a class="btn btn-dark fs-5 " href="/datosNego" style="width:185px">Editar</a></div>
+      <div class="my-3 mx-auto"><a class="btn btn-dark fs-5" href="/categoria" style="width:185px">Registrar producto</a></div>
+      <div class="my-3 mx-auto"><a class="btn btn-dark fs-5" href="{{route('listaproducto')}}" style="width:185px">Ver productos</a></div>
     </div>
 
 
