dd4e10e5776bcc6456e90ca4e53a4745380e6cb6
VilMF <vilmamamaniflores@gmail.com>
2022-11-08 21:48:10 -0400

    asd

--- resources/views/Proveedor/Paginaproveedor.blade.php
+++ resources/views/Proveedor/Paginaproveedor.blade.php
@@ -11,7 +11,7 @@
             <div class="my-3 mx-auto"><a class="btn btn-dark fs-5 " href="/datosNego" style="width:185px">Editar</a></div>
             <div class="my-3 mx-auto"><a class="btn btn-dark fs-5" href="/categoria" style="width:185px">Registrar producto</a></div>
             <div class="my-3 mx-auto"><a class="btn btn-dark fs-5" href="{{route('listaproducto')}}" style="width:185px">Ver productos</a></div>
-            <div class="my-3 mx-auto"><a class="btn btn-dark fs-5" href="/novedades" style="width:185px">Cerrar sesi&oacute;n</a></div>
+            <div class="my-3 mx-auto"><a class="btn btn-dark fs-5" href="/novedades" style="width:185px">Cerrar sesi&ooacute;n</a></div>
             </div>
 
 
