92ae0e350c9d3a2dffda8d1bde3368a3554abec0
rodolfokevinperez@gmail.com <rodolfokevinperez@gmail.com>
2022-11-08 20:41:04 -0400

    VistaProveedor

--- resources/views/Proveedor/Paginaproveedor.blade.php
+++ resources/views/Proveedor/Paginaproveedor.blade.php
@@ -1,25 +1,30 @@
-@extends('layouts.index')
+@extends('layouts.template')
 
-@section('body')
+    @section('content')
+        @extends('layouts.index')
 
-<div class="d-flex justify-content-around " style="padding-top: 15px;" >
-<div class="container-fluid">
-  <div class="row " style="min-height:600px">
+        @section('body')
 
-    <div class="btn-vertical col d-flex  flex-column align-self-center   d-flex justify-content-start" style="padding-right:15em">
-      <div class="my-3 mx-auto"><a class="btn btn-dark fs-5 " href="/datosNego" style="width:185px">Editar</a></div>
-      <div class="my-3 mx-auto"><a class="btn btn-dark fs-5" href="/categoria" style="width:185px">Registrar producto</a></div>
-      <div class="my-3 mx-auto"><a class="btn btn-dark fs-5" href="{{route('listaproducto')}}" style="width:185px">Ver productos</a></div>
-    </div>
+        <div class="d-flex justify-content-around " style="padding-top: 15px;" >
+        <div class="container-fluid">
+        <div class="row " style="min-height:600px">
 
-
-    <div class="col">
-      @yield('Contenido')
-    </div>
-  </div>
-</div>
+            <div class="btn-vertical col d-flex  flex-column align-self-center   d-flex justify-content-start" style="padding-right:15em">
+            <div class="my-3 mx-auto"><a class="btn btn-dark fs-5 " href="/datosNego" style="width:185px">Editar</a></div>
+            <div class="my-3 mx-auto"><a class="btn btn-dark fs-5" href="/categoria" style="width:185px">Registrar producto</a></div>
+            <div class="my-3 mx-auto"><a class="btn btn-dark fs-5" href="{{route('listaproducto')}}" style="width:185px">Ver productos</a></div>
+            </div>
 
 
-<div></div>
-</div>
+            <div class="col">
+            @yield('Contenido')
+            </div>
+        </div>
+        </div>
+
+
+        <div></div>
+        </div>
+        @endsection
+    </section>
 @endsection
