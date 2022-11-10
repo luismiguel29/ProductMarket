a44ca44da445e76615ed535f06fd02c722af800d
VilMF <vilmamamaniflores@gmail.com>
2022-11-10 19:32:11 -0400

    cambio0 paginacion en movil

--- resources/views/vendor/pagination/bootstrap-5.blade.php
+++ resources/views/vendor/pagination/bootstrap-5.blade.php
@@ -5,11 +5,11 @@
                 {{-- Previous Page Link --}}
                 @if ($paginator->onFirstPage())
                     <li class="page-item disabled" aria-disabled="true">
-                        <span class="page-link">@lang('pagination.previous')</span>
+                        <span class="page-link">&lsaquo;</span>
                     </li>
                 @else
                     <li class="page-item">
-                        <a class="page-link" href="{{ $paginator->previousPageUrl() }}" rel="prev">@lang('pagination.previous')</a>
+                        <a class="page-link" href="{{ $paginator->previousPageUrl() }}" rel="prev">&lsaquo;</a>
                     </li>
                 @endif
 
