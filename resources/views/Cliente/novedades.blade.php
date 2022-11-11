6ad241eb11a55af948720bff586bf0ae48f3a905
ypelaez <yohana.pelaez.cruz@gmail.com>
2022-11-11 06:19:45 -0400

    update

--- resources/views/Cliente/novedades.blade.php
+++ resources/views/Cliente/novedades.blade.php
@@ -76,11 +76,11 @@
                     @foreach ($a as $item)
                     <div class="carousel-item active" data-bs-interval="5000" >
                         <div class="cards-wrapper" >
-                            <div class="card" style="background-color: #E3E9E6" style="width: 15rem;">
+                            <div class="card" style="background-color: #E3E9E6">
                                 <div class=" row row-cols-1 g-5">
                                     @foreach ($item as $item1)
                                         <div class="col-12 col-md-6 col-lg-4 ">
-                                            <div class="card" >
+                                            <div class="card" style="width: 15rem;">
                                                 <div class="card-header c-header">
                                                     <h5 class="text-center" style="color: white">SUPER OFERTA</h5>
                                                 </div>
