95d0dfe5dbc0fe911c4f956ccbc24e6351134d8c
ypelaez <yohana.pelaez.cruz@gmail.com>
2022-11-11 06:43:35 -0400

    update

--- resources/views/Cliente/novedades.blade.php
+++ resources/views/Cliente/novedades.blade.php
@@ -71,12 +71,13 @@
 </html>
 
     <section class="container">
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"
+            <div id="carouselExampleControls"  class="carousel carousel-dark slide car-out" data-ride="carousel"{{--  data-bs-ride="carousel" --}}>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"
                     data-bs-slide="prev">
                     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                     <span class="visually-hidden">Previous</span>
                 </button>
-            <div id="carouselExampleControls"  class="carousel carousel-dark slide car-out" data-ride="carousel"{{--  data-bs-ride="carousel" --}}>
+                
                 <div class="carousel-inner">
                     @foreach ($a as $item)
                     <div class="carousel-item active" data-bs-interval="5000" >
@@ -112,11 +113,12 @@
                    @endforeach
 
                 </div>
-            </div>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls"
+               
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls"
                     data-bs-slide="next">
                     <span class="carousel-control-next-icon" aria-hidden="true"></span>
                     <span class="visually-hidden">Next</span>
                 </button>
+            </div>
 </section>
 @endsection
