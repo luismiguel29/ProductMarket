a6676431eeba5bb40c7a96433ae0e038ec79d04a
ypelaez <yohana.pelaez.cruz@gmail.com>
2022-11-12 17:06:49 -0400

    u´date

--- resources/views/Cliente/novedades.blade.php
+++ resources/views/Cliente/novedades.blade.php
@@ -75,8 +75,8 @@
 
   
     <!--------------------------------------------------->
+    <link rel="stylesheet" href="{{ asset('slider/css/swiper-bundle.min.css') }}">
     
-
     <section class="container-sm">
         <div class="container container-novedades"> 
             <div id="carouselExampleControls"  class="carousel carousel-dark slide car-out" data-ride="carousel"{{--  data-bs-ride="carousel" --}}>
@@ -150,123 +150,5 @@
 
 
 {{-- ---------------------------------------------------------------- --}}
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="https://getbootstrap.com/docs/5.2/assets/css/docs.css" rel="stylesheet">
-    <title>Bootstrap Example</title>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
-    <link href="index.css" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('editarAll/style.css') }}">
-</head>
-  
-  <body>
-    <div class="container">
 
-    <!-- Example Code -->
-    
-    <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
-      <div class="carousel-inner">
-        <div class="carousel-item active" data-bs-interval="2000">
-            <div class="cards-wrapper">
-                <div class="card">
-                  <div class="card-header c-header">SUPER OFERTA</div>
-                  <img src="..." class="card-img-top" alt="...">
-                  <div class="card-body">
-                    <h5 class="card-title">TITULO</h5>
-                  </div>
-                  <div class="card-footer c-footer">PRECIO</div>
-                </div>
-                <div class="card">
-                  <div class="card-header c-header">SUPER OFERTA</div>
-                  <img src="..." class="card-img-top" alt="...">
-                  <div class="card-body">
-                    <h5 class="card-title">TITULO</h5>
-                  </div>
-                  <div class="card-footer c-footer">PRECIO</div>
-                </div>
-                <div class="card">
-                  <div class="card-header c-header">SUPER OFERTA</div>
-                  <img src="..." class="card-img-top" alt="...">
-                  <div class="card-body">
-                    <h5 class="card-title">TITULO</h5>
-                  </div>
-                  <div class="card-footer c-footer">PRECIO</div>
-                </div>
-              </div>    
-        </div>
-        <div class="carousel-item" data-bs-interval="2000">
-            <div class="cards-wrapper">
-                <div class="card">
-                  <div class="card-header c-header">SUPER OFERTA</div>
-                  <img src="..." class="card-img-top" alt="...">
-                  <div class="card-body">
-                    <h5 class="card-title">TITULO</h5>
-                  </div>
-                  <div class="card-footer c-footer">PRECIO</div>
-                </div>
-                <div class="card">
-                  <div class="card-header c-header">SUPER OFERTA</div>
-                  <img src="..." class="card-img-top" alt="...">
-                  <div class="card-body">
-                    <h5 class="card-title">TITULO</h5>
-                  </div>
-                  <div class="card-footer c-footer">PRECIO</div>
-                </div>
-                <div class="card">
-                  <div class="card-header c-header">SUPER OFERTA</div>
-                  <img src="..." class="card-img-top" alt="...">
-                  <div class="card-body">
-                    <h5 class="card-title">TITULO</h5>
-                  </div>
-                  <div class="card-footer c-footer">PRECIO</div>
-                </div>
-              </div>    
-        </div>
-        <div class="carousel-item" data-bs-interval="2000">
-            <div class="cards-wrapper">
-                <div class="card">
-                  <div class="card-header c-header">SUPER OFERTA</div>
-                  <img src="..." class="card-img-top" alt="...">
-                  <div class="card-body">
-                    <h5 class="card-title">TITULO</h5>
-                  </div>
-                  <div class="card-footer c-footer">PRECIO</div>
-                </div>
-                <div class="card">
-                  <div class="card-header c-header">SUPER OFERTA</div>
-                  <img src="..." class="card-img-top" alt="...">
-                  <div class="card-body">
-                    <h5 class="card-title">TITULO</h5>
-                  </div>
-                  <div class="card-footer c-footer">PRECIO</div>
-                </div>
-                <div class="card">
-                  <div class="card-header c-header">SUPER OFERTA</div>
-                  <img src="..." class="card-img-top" alt="...">
-                  <div class="card-body">
-                    <h5 class="card-title">TITULO</h5>
-                  </div>
-                  <div class="card-footer c-footer">PRECIO</div>
-                </div>
-              </div>    
-        </div>
-      </div>
-      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
-      </button>
-      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
-      </button>
-    </div>
-    
-    <!-- End Example Code -->
-</div>
-  </body>
-</html>
 @endsection
