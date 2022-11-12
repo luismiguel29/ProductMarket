40eaa6fc089038f2a7cba35fef4614a32464b86a
ypelaez <yohana.pelaez.cruz@gmail.com>
2022-11-11 22:58:58 -0400

    update

--- resources/views/Cliente/novedades.blade.php
+++ resources/views/Cliente/novedades.blade.php
@@ -18,7 +18,7 @@
         {{-- <link rel="stylesheet" href="css/style.css"> --}}
         <link rel="stylesheet" href="{{ asset('slider/css/swiper-bundle.min.css') }}">
        {{--  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"/> --}}
-        <link rel="stylesheet" href="{{ asset('novedades/novedades.css') }}">
+       {{--  <link rel="stylesheet" href="{{ asset('novedades/novedades.css') }}"> --}}
     </head>
 
     <body>
