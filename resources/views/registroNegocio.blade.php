d572b0f7c9dad61f96433e8f9be044ac522f3dac
Andrea Pelaez <andrea.pelaez436@gmail.com>
2022-11-12 23:16:01 -0400

    prueba

--- resources/views/registroNegocio.blade.php
+++ resources/views/registroNegocio.blade.php
@@ -55,8 +55,8 @@
                     @if (session('mensajeHor')) 
                         <div class="alert alert-warning alert-dismissible fade show" role="alert">
                             <strong>{{ session('mensajeHor') }}</strong>
-                            <button type="button" class="btn-close" data-bs-dismiss="alert"
-                                aria-label="Close"></button>
+                            <!--<button type="button" class="btn-close" data-bs-dismiss="alert"
+                                aria-label="Close"></button>-->
                         </div>
                     @endif
 
