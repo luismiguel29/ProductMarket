b6a7f6db58290a3e8e20965b68c3bd3468930c3e
luismiguel29 <rojashuallcoluismiguel29@gmail.com>
2022-11-15 21:49:23 -0400

    asd

--- app/Http/Controllers/ProductoController.php
+++ app/Http/Controllers/ProductoController.php
@@ -62,7 +62,7 @@ class ProductoController extends Controller
         ]);
 
         if ($validator->fails()) {
-            return redirect('categoria')->withErrors($validator)->withInput();
+            return redirect('categoria')->with('alerta', 'Debe subir un archivo de imagen png,jpg de 500x500 o 600x600')->withInput();
         } else {
             $url = Cloudinary::upload($request->file('url_img')->getRealPath())->getSecurePath();
 
