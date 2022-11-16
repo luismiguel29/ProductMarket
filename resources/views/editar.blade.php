c6027d0171a2fe3be7538704b53629a741e24530
Andrea Pelaez <andrea.pelaez436@gmail.com>
2022-11-16 15:17:54 -0400

    prueba

--- resources/views/editar.blade.php
+++ resources/views/editar.blade.php
@@ -50,7 +50,7 @@
 
     <!-- id="espacio"-->
     <div class="d-flex justify-content-evenly" style="padding-top: 50px;">
-        <section class="d-flex flex-column  align-self-center gap-4">
+        <section class="d-flex flex-column  align-self-center gap-4" class="seccion1">
             <button class="btn btn-dark fs-5 btnb">Editar</button>
             <a href="categoria" class="btn btn-dark fs-5 btnb">Registrar producto</a>
             <a href="proveedor/listaproducto" class="btn btn-dark fs-5 btnb">Ver productos</a>
@@ -58,7 +58,7 @@
         </section>
 
 
-        <div class="card mb-5" style="width: 25rem;">
+        <div class="card mb-5" style="width: 25rem;" class="seccion2">
             <div class="card-header c-header" style="background-color: #FFD507; padding:25px"> </div>
             <div class="card-body text-dark card-custom-p">
 
