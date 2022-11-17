b7e4bd0b57b95dd3a296a64ee2a13e79b933c100
VilMF <vilmamamaniflores@gmail.com>
2022-11-17 17:02:16 -0400

    boton eliminar en columna acciones

--- resources/views/Proveedor/Verlistaproductos.blade.php
+++ resources/views/Proveedor/Verlistaproductos.blade.php
@@ -44,7 +44,7 @@
                 @endif
                 </td>
               <td><img style="max-width: 100px; max-heidth:100px" src="{{$producto->url}}"  alt=""></td>
-              <td><a class="btn"><i data-feather="edit"></i></a></td>
+              <td><a class="btn"><i data-feather="edit"></i></a><a class="btn"><i data-feather="trash-2"></i></a></td>
 
             </tr>
             @endforeach
