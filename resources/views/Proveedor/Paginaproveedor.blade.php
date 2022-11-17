0e58f227535289f045fd641b83b45822bc4d9c86
VilMF <vilmamamaniflores@gmail.com>
2022-11-17 16:32:19 -0400

    asdf1

--- resources/views/Proveedor/Paginaproveedor.blade.php
+++ resources/views/Proveedor/Paginaproveedor.blade.php
@@ -6,10 +6,10 @@
         <div class="d-flex justify-content-around " style="padding-top: 15px;" >
         <div class="container-fluid">
         <div class="row " style="min-height:600px">
-            <div class="col-sm-8 order-lg-2">
+            <div class="col-sm-8 order-md-2">
                 @yield('Contenido')
             </div>
-            <div class="col-sm-4 order-lg-1 d-flex  flex-column align-items-center justify-content-center" style="padding:0">
+            <div class="col-sm-4 order-md-1 d-flex  flex-column align-items-center justify-content-center" style="padding:0">
             <div class="my-3 mx-auto"><a class="btn btn-dark fs-5 " href="/datosNego" style="width:185px">Editar</a></div>
             <div class="my-3 mx-auto"><a class="btn btn-dark fs-5" href="/categoria" style="width:185px">Registrar producto</a></div>
             <div class="my-3 mx-auto"><a class="btn btn-dark fs-5" href="{{route('listaproducto')}}" style="width:185px">Ver productos</a></div>
