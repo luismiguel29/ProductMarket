7c7fcb41461654477788e2b4aad26ca70574d924
rodolfokevinperez@gmail.com <rodolfokevinperez@gmail.com>
2022-11-21 11:34:01 -0400

    IMGS

--- resources/views/Cliente/listaNegocios.blade.php
+++ resources/views/Cliente/listaNegocios.blade.php
@@ -23,7 +23,7 @@
 
                 <header class="bann">
                     <div class="logo">
-                        <img src="\imagenes\tienda.png" width="100px">
+                        <img src="\Imagenes\tienda.png" width="100px">
                         <h2 class="nombreCat">
                             <strong>Negocios</strong>
                         </h2>
