e0d429b4c8c6c5eaba45f1dc63253cfa5b04d3af
VilMF <vilmamamaniflores@gmail.com>
2022-11-25 18:20:52 -0400

    cambio de estios en modal

--- resources/views/Proveedor/Verlistaproductos.blade.php
+++ resources/views/Proveedor/Verlistaproductos.blade.php
@@ -56,11 +56,11 @@
           </tbody>
         </table>
         <!-- Modal -->
-          <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal fade" id="deleteModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
             <div class="modal-dialog">
               <div class="modal-content">
                 <div class="modal-header">
-                  <h5 class="modal-title" id="exampleModalLabel">Eliminar Producto</h5>
+                  <h1 class="modal-title fs-5" id="exampleModalLabel">Eliminar Producto</h1>
                   <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
