34523e0688aa9cc91553260c5bdf026a77ecefb7
VilMF <vilmamamaniflores@gmail.com>
2022-11-25 17:49:02 -0400

    definicion de variable en campo fecha inicio promo

--- resources/views/registrar.blade.php
+++ resources/views/registrar.blade.php
@@ -111,7 +111,7 @@
                             <div class="mb-3">
                                 <label for="exampleInputPassword1" class="form-label">Fecha inicio promo</label>
                                 <input type="date" class="form-control" name="fechainiciopromo" max="2023-01-22"
-                                    value="{{ old('fechainiciopromo') }}" required=""
+                                    value="{{isset($producto)? $producto->fechainicio: old('fechainiciopromo') }}" required=""
                                     placeholder="Introduce una fecha" required min=<?php $hoy = date('Y-m-d');
                                     echo $hoy; ?> id="fvenc">
                             </div>
