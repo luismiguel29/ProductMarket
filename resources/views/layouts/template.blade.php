75e1ff1242bffb7106858c6920f31081a8d35845
Andrea Pelaez <andrea.pelaez436@gmail.com>
2022-11-29 16:52:38 -0400

    prueba

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -147,13 +147,13 @@
                             <h5><strong>TOTAL</strong></h5>
                             <H5><strong>Bs. {{ $total }}</strong></H5>
                         </div>
-                        <!--<div class="botonCompra">
+                        <div class="botonCompra">
                             <button type="submit" class="btn btn-dark fs-5 {{-- btn-block --}}" style="width: 300px"
                                 style="font-size: 60px" --}}>Finalizar Compra</button>
-                        </div>-->
-                        <a href="/endC"
+                        </div>
+                        <!--<a href="/endC"
                             class="btn btn-dark fs-5 {{-- btn-block --}}"
-                            style="width: 300px" style="font-size: 60px">Finalizar Compra</a>
+                            style="width: 300px" style="font-size: 60px">Finalizar Compra</a>-->
 
                     </div>
                 @endif
