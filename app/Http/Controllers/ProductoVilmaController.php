d33b4703ba04c5026802e9883ff64b1637b20fea
VilMF <vilmamamaniflores@gmail.com>
2022-11-28 20:49:01 -0400

    Actualizar debe ser null

--- app/Http/Controllers/ProductoVilmaController.php
+++ app/Http/Controllers/ProductoVilmaController.php
@@ -111,7 +111,7 @@ class ProductoVilmaController extends Controller
     public function update(Request $request, $id)
     {
         $validator = Validator::make($request->all(), [
-            'url_img' => 'required|image|mimes:png,jpg|dimensions:min_width=500,min_height=500,max_width=600,max_height=600',
+            'url_img' => 'nullable|image|mimes:png,jpg|dimensions:min_width=500,min_height=500,max_width=600,max_height=600',
         ]);
 
         if ($validator->fails()) {
