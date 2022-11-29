39c3a085383d2cd195038fe482549fe6cc86fdf8
Andrea Pelaez <andrea.pelaez436@gmail.com>
2022-11-29 17:53:06 -0400

    prueba

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -89,9 +89,7 @@
 
 
                 @if($total == 0)
-                    <div class="col-md-4 d-md-none justify-content-center">
-                        <img src="{{ asset('editarAll/img/carritovacio.png') }}" class="my-4">
-                    </div>
+                    <img src="{{ asset('editarAll/img/carritovacio.png') }}" style="width: 300px; height: 300px;">
 
                     <p class="fs-3 text-center">Tu carrito está vacio</p>
 
