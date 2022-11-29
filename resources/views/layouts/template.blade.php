34687a5a7f8516198958190ddb1461cc02d2d8d8
Andrea Pelaez <andrea.pelaez436@gmail.com>
2022-11-29 16:36:44 -0400

    prueba

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -62,7 +62,7 @@
                         </li>
                 </div>
             </div>
-
+            <!------------------------------------------------------------------------->
             <div>
                 <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas"
                     data-bs-target="#offcanvasNavbarRR" aria-controls="offcanvasNavbaR">
@@ -86,66 +86,11 @@
                     <span type="submit" class="fa-sharp fa-solid fa-circle-xmark fa-2x"
                         data-bs-dismiss="offcanvas"></span>
                 </div>
-                <div class="offcanvas-body">
-                    <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-                        <li class="nav-item">
-                            @foreach ($auxarr as $item)
-                                <div class="card mb-1" style="max-width:540px;">
-                                    <div class="d-flex justify-content-center row g-0">
-                                        <div class="col-sm-4">
-                                            <img src="{{ $item->url }}" class="card-img-top">
-                                        </div>
-                                        <div class="col-md-4">
-                                            <div class="card-body">
-                                                {{--  <span class="card-title"><b>Stock {{ $item->stock}}</b></span> --}}
-                                                <h6 class="card-title"><b>{{ $item->nombre }}</b></h6>
-                                                {{--  <span style="color:#5D5D5D">Antes <s>Bs.{{ $item->precio }}</s></span> --}}
-                                                <span>Ahora <strong>Bs. {{ $item->preciodesc }}</strong></spa </div>
-                                                    <a href=""></a>
-                                            </div>
-                                        </div>
-                                        <div class="col-md-4 align-self-center">
-                                            <div class="d-flex justify-content-around">
-                                                <a href="/decrementar/{{ $item->idcarrito }}"
-                                                    class="fa-solid fa-square-minus fa-2x"
-                                                    style="text-decoration: none;color:black"></a>
-                                                <span style="font-size:20px">{{ $item->cantidad }}</span>
-                                                <a href="/incrementar/{{ $item->idcarrito }}"
-                                                    class="fa-solid fa-square-plus fa-2x"
-                                                    style="text-decoration: none;color:black"></a>
-
-
-                                                <form action="{{ route('carrito.destroy', $item->idcarrito) }}"
-                                                    method="post">
-                                                    @csrf
-                                                    @method('DELETE')
-                                                    <button type="submit" class="fa-solid fa-trash fa-2x"
-                                                        value="Eliminar" style="border:none"> </button>
-                                                </form>
-
-                                            </div>
-                                        </div>
-
-                                    </div>
-                                </div>
-                        </li>
-                        @endforeach
-
-
-                    </ul>
-
-                    <div class="d-flex justify-content-around">
-                        <h5><strong>TOTAL</strong></h5>
-                        <H5><strong>Bs. {{ $total }}</strong></H5>
-                    </div>
-                    <div class="botonCompra">
-                        <button type="submit" class="btn btn-dark fs-5 {{-- btn-block --}}" style="width: 300px"
-                            style="font-size: 60px" --}}>Finalizar Compra</button>
-                    </div>
-                </div>
+                
 
 
             </div>
+            <!------------------------------------------------------------------------->
     </nav>
 
 
