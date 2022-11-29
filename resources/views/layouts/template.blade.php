783599bcc799f5561ec26319c79c05bf0fe97112
ypelaez <yohana.pelaez.cruz@gmail.com>
2022-11-29 14:49:38 -0400

    update

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -63,8 +63,8 @@
                 </div>
             </div>
 
-
-            <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbarRR"
+        
+            <div class="offcanvas offcanvas-end" tabindex="{{-- -1 --}}1" id="offcanvasNavbarRR"
                 aria-labelledby="offcanvasNavbarLabel">
                 <div class=" header-carrito d-flex justify-content-between offcanvas-header">
                     <div class="d-flex flex-column center">
