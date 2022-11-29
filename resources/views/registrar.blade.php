f360a7af2c10c5f5ec8b1d33d264d1c2042f8e53
VilMF <vilmamamaniflores@gmail.com>
2022-11-28 21:20:59 -0400

    agregar route a form

--- resources/views/registrar.blade.php
+++ resources/views/registrar.blade.php
@@ -44,7 +44,7 @@
         
                     <h5 class="text-center fs-5">Información del producto</h5>
                         {{-- @include('components.flash_alerts') --}}
-                        <form action="{{ isset($producto)? route('producto.update',['id'=>$producto->idproducto]): }}" method="post" enctype="multipart/form-data">
+                        <form action="{{ isset($producto)? route('producto.update',['id'=>$producto->idproducto]): route('producto.store') }}" method="post" enctype="multipart/form-data">
                             @csrf
                             <!--<div class=" mb-3 input-group">
                                 <input type="file" class="form-control" id="inputImg">
