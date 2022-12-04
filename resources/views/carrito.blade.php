4914cca7c4cabb7743fb6234ae92da2554367799
Andrea Pelaez <andrea.pelaez436@gmail.com>
2022-12-04 17:53:43 -0400

    prueba

--- resources/views/carrito.blade.php
+++ resources/views/carrito.blade.php
@@ -117,16 +117,37 @@
                         class="btn btn-dark fs-5 {{-- btn-block --}}" style="width: 300px" 
                         style="font-size: 60px" data-bs-toggle="modal" data-bs-target="#borrar">Finalizar Compra</a>
                 </div>
-
-                <button type="button" class="btn btn-primary btn-sm" data-bs-toggle="modal" data-bs-target="#borrar">
-                    textooo
-                </button>
                 @include('finalizar')
 
             </div>
         @endif
     </div>
     {{-- FIN CARRITO DE COMPRAS --}}
+
+    <!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+  Launch demo modal
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+
 
 </body>
 
