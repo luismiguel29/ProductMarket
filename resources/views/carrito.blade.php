670728ac2ccc14d7de55128b33ac2b32d5f3227e
Andrea Pelaez <andrea.pelaez436@gmail.com>
2022-12-04 17:57:55 -0400

    prueba

--- resources/views/carrito.blade.php
+++ resources/views/carrito.blade.php
@@ -121,15 +121,11 @@
 
             </div>
         @endif
-    </div>
-    {{-- FIN CARRITO DE COMPRAS --}}
+        <!---------------------->
+        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+            Launch demo modal
+        </button>
 
-    <!-- Button trigger modal -->
-<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-  Launch demo modal
-</button>
-
-<!-- Modal -->
 <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
@@ -142,13 +138,13 @@
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
       </div>
     </div>
   </div>
 </div>
-
-
+        <!---------------------->
+    </div>
+    {{-- FIN CARRITO DE COMPRAS --}}
 </body>
 
 </html>
