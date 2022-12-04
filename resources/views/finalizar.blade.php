407197e472f549598f9ff73a82ed3fa1a3b37bb6
Andrea Pelaez <andrea.pelaez436@gmail.com>
2022-12-04 17:17:43 -0400

    prueba

--- resources/views/finalizar.blade.php
+++ resources/views/finalizar.blade.php
@@ -2,18 +2,26 @@
 
 <div class="modal fade" id="borrar" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog">
+    <div class="botonCompra">
+        
+    
         <div class="modal-content">
             <div class="modal-header">
-                <h1 class="modal-title fs-5" id="exampleModalLabel">Finalizar Compra</h1>
+                <h1 class="modal-title fs-5" id="exampleModalLabel">Finalizar Compra titulo</h1>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
                 Desea finalizar la compra
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Save changes</button>
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                <a href="/endC"
+                    class="btn btn-dark fs-5 {{-- btn-block --}}" 
+                    style="width: 300px" style="font-size: 60px">Finalizar Compra</a>
             </div>
         </div>
+
+    </div>
+
     </div>
 </div>
