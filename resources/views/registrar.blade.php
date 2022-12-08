5b494ca6d375411d3828654938b7ffcc92107493
VilMF <vilmamamaniflores@gmail.com>
2022-12-08 12:21:59 -0400

    cambio1 en campo nombre

--- resources/views/registrar.blade.php
+++ resources/views/registrar.blade.php
@@ -62,6 +62,17 @@
                                         @endforeach
                                     </div>
                                 @endif --}}
+                                @if ($errors->any())
+                                    <div class="alert alert-danger">
+                                        <ul>
+                                            @foreach ($errors->all() as $error)
+                                                <li>{{ $error }}</li>
+                                            @endforeach
+                                        </ul>
+                                    </div>
+                                @endif
+
+
                                 @if (session('message'))
                                     <div class="alert alert-success alert-dismissible fade show" role="alert">
                                         <strong>{{ session('message') }}</strong>
