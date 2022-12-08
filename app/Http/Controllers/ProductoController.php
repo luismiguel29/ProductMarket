4269a40997438c1a0dbe9fcc3c5b2975698feee3
VilMF <vilmamamaniflores@gmail.com>
2022-12-08 12:02:49 -0400

    bug cantidad de caracteres restriccion

--- app/Http/Controllers/ProductoController.php
+++ app/Http/Controllers/ProductoController.php
@@ -42,9 +42,11 @@ class ProductoController extends Controller
         /* DB::select('call regprod(?,?,?,?,?,?,?)',array($request->idneg,$request->nombreprod,
         $request->precionormal,$request->preciodesc,$request->stockprod,$request->fechavenprod,$request->descripprod));
        */
-        $validator = Validator::make($request->all(), [
+        $validator = $request->validate([
             //'url_img' => 'required|image|mimes:png,jpg|dimensions:min_width=500,min_height=500,max_width=600,max_height=600',
             'url_img' => 'required|image|mimes:png,jpg|dimensions:max_width=600,max_height=600',
+            'nombreprod'=>'max:50'
+        
         ]);
 
         $pre = $request->input('precio');
