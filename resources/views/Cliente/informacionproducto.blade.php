70048b7d0e1057031a50c3fcd7fd1794e9a26dc4
ypelaez <yohana.pelaez.cruz@gmail.com>
2022-12-10 10:24:39 -0400

    update

--- resources/views/Cliente/informacionproducto.blade.php
+++ resources/views/Cliente/informacionproducto.blade.php
@@ -104,7 +104,7 @@
                             <hr class="separador">
                             <h5>{{ $negocio->nombre }}</h5>
                             <span class="fa-sharp fa-solid fa-location-dot" ></span>
-                            <span >{{ $negocio->direccion }}</span>
+                            <span style="font-weight: 500">{{ $negocio->direccion }}</span>
                             <h5><span style="color: #7D7D7D">Horario: {{ $negocio->horarioinicio }} -
                                     {{ $negocio->horariofin }}</span> </h5>
 
