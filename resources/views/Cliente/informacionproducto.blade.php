b538eec7c44cc897a799b774867da6d0c8bd6e53
ypelaez <yohana.pelaez.cruz@gmail.com>
2022-12-10 10:16:26 -0400

    update

--- resources/views/Cliente/informacionproducto.blade.php
+++ resources/views/Cliente/informacionproducto.blade.php
@@ -103,7 +103,7 @@
                             <h5><span style="color: #7D7D7D">Stock: {{ $producto->stock }}</span></h5>
                             <hr class="separador">
                             <h5>{{ $negocio->nombre }}</h5>
-                            <i class="fa-sharp fa-solid fa-location-dot fa-2x" style = "float: left"></i>
+                            <i class="fa-sharp fa-solid fa-location-dot fa-5x" style = "float: left"></i>
                             <h5 >{{ $negocio->direccion }}</h5>
                             <h5><span style="color: #7D7D7D">Horario: {{ $negocio->horarioinicio }} -
                                     {{ $negocio->horariofin }}</span> </h5>
