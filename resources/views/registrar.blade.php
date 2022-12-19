a0f1e5c5ee7e45809e40b450427b4e32b10a75ac
luismiguel29 <rojashuallcoluismiguel29@gmail.com>
2022-12-19 09:26:19 -0400

    asd

--- resources/views/registrar.blade.php
+++ resources/views/registrar.blade.php
@@ -75,7 +75,7 @@
                                 @if (session('message'))
                                     <div class="alert alert-success alert-dismissible fade show" role="alert">
                                         <span id="confirmacion">
-                                            <strong>{{ session('message') }}</strong>
+                                            {{ session('message') }}
                                         </span>
                                         <button type="button" class="btn-close" data-bs-dismiss="alert"
                                             aria-label="Close"></button>
